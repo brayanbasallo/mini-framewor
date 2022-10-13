d3f7af0547af84c099dfa3223272540f3ea5b400
Brayan Basallo Soto <basallo17@gmail.com>
2022-10-12 21:34:23 -0500

    se crea el Proxy para utilizar las trampas

--- js/code.js
+++ js/code.js
@@ -1,11 +1,23 @@
 class PlatziReactive {
     constructor(options) {
         this.origen = options.data()
+
+
+        //destino
+        this.$data = new Proxy(this.origen, {
+            get(target, name) {
+                if (name in target){
+                    return target[name]
+                }
+                console.error(`La propiedad ${name} no existe`)
+                return ""
+            }
+        })
     }
 
     mount(){
         document.querySelectorAll("*[p-text]").forEach(el=>{
-            this.pText(el, this.origen, el.getAttribute("p-text"));
+            this.pText(el, this.$data, el.getAttribute("p-text"));
         })
     }
 
