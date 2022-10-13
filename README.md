c134516bb7d38fc67e6ced122cb12447c9c35434
Brayan Basallo Soto <basallo17@gmail.com>
2022-10-12 22:00:30 -0500

    Se agrega Reflect para mejorar la legibilidad de nuestro codigo

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 I'm learning about reactivity in a platzi course 💚.
 
 A mini framework based on the vue syntax is developed
+
+### Reflect
+This is very interesting, about this [here](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Reflect)
--- js/code.js
+++ js/code.js
@@ -6,11 +6,16 @@ class PlatziReactive {
         //destino
         this.$data = new Proxy(this.origen, {
             get(target, name) {
-                if (name in target){
-                    return target[name]
+                if (Reflect.has(target, name)){
+                    // return target[name]
+                    Reflect.get(target, name)
                 }
                 console.error(`La propiedad ${name} no existe`)
                 return ""
+            },
+            set(target, name, value){
+                console.log(target, name , value)
+
             }
         })
     }
@@ -19,12 +24,23 @@ class PlatziReactive {
         document.querySelectorAll("*[p-text]").forEach(el=>{
             this.pText(el, this.$data, el.getAttribute("p-text"));
         })
+        document.querySelectorAll("*[p-model]").forEach(el=>{
+            const NAME = el.getAttribute("p-model")
+            this.pModel(el, this.$data, NAME)
+
+            el.addEventListener("input", ()=>{
+                //this.$data[NAME] = el.value
+                Reflect.set(this.$data, NAME, el.value)
+            })
+        })
     }
 
     pText(el, target, name){
-        el.innerText = target[name];
+        el.innerText = Reflect.get(target, name);
     }
-    pModel(){}
+    pModel(el, target, name){
+        el.value = Reflect.get(target, name)
+    }
 }
 
 var platzi = {
