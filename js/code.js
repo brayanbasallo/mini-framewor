6b73310c5d6f0c248f47acffe51eda9c8cf120a3
Brayan Basallo Soto <basallo17@gmail.com>
2022-10-12 21:20:38 -0500

    commit inicial

---
+++ js/code.js
@@ -0,0 +1,22 @@
+class PlatziReactive {
+    constructor(options) {
+        this.origen = options.data()
+    }
+
+    mount(){
+        document.querySelectorAll("*[p-text]").forEach(el=>{
+            this.pText(el, this.origen, el.getAttribute("p-text"));
+        })
+    }
+
+    pText(el, target, name){
+        el.innerText = target[name];
+    }
+    pModel(){}
+}
+
+var platzi = {
+    createApp(options) {
+        return new PlatziReactive(options)
+    }
+}
